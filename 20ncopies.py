8601cdfe66a07f22c2c5ad147d48f744661b8821
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 21:34:32 +0530

    n copies of a string

---
+++ 20ncopies.py
@@ -0,0 +1,10 @@
+text= str(input("enter string : "))
+n= int(input("enter the no of copies :"))
+def n_copies(text,n):
+    return n*text
+print(n_copies(text,n))
+
+#output
+'''enter string : sai
+enter the no of copies :3
+saisaisai'''

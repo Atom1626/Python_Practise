3f70fd4798ba5c3d9c6f71b966559ea697fc7b72
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 15:53:32 +0530

    Python version

---
+++ pyversion.py
@@ -0,0 +1,11 @@
+import sys
+print("python version")
+print(sys.version)
+print("python version info")
+print(sys.version_info)
+
+# output
+"""python version
+3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)]
+python version info
+sys.version_info(major=3, minor=10, micro=6, releaselevel='final', serial=0)""

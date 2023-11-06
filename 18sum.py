2d5e031d036292bf9f50ab7b53257ab8348e3dc4
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 21:12:04 +0530

    sum

---
+++ 18sum.py
@@ -0,0 +1,18 @@
+n1 = float(input("enter 1st value"))
+n2 = float(input("enter 2st value"))
+n3 = float(input("enter 3st value"))
+if n1 == n2 == n3:
+    print((n1+n2+n2)*3)
+else:
+    print(n1+n2+n3)
+
+#output
+'''enter 1st value10
+enter 2st value10
+enter 3st value10
+90.0
+
+enter 1st value1
+enter 2st value2
+enter 3st value3
+6.0'''

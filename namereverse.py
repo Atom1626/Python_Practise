0af7ef4974c1d7da64896d476b829f6e536cdbfe
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 16:24:10 +0530

    accepts the user's first and last name and prints them in reverse order with a space between them

---
+++ namereverse.py
@@ -0,0 +1,8 @@
+fname = str(input("enter first name  : "))
+lname = str(input("enter last name : "))
+print(lname +" "+fname)
+
+#output
+'''enter first name  : sai
+   enter last name : siva
+   siva sai'''

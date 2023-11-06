94ade612d38f9fffbd390fcb5882d45b89c5fc1d
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 20:51:28 +0530

    difference

---
+++ 16absdiff.py
@@ -0,0 +1,13 @@
+n= int(input("enter n value :"))
+def diff(n):
+    if n<=17:
+        return 17-n
+    else:
+        return (n-17)*2
+print(diff(n))
+
+#output
+'''enter n value :22
+10
+enter n value :14
+3'''

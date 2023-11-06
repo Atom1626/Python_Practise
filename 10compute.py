63f1931a2f94a7dc53304fe3dba78820ff0a1d3f
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 17:18:21 +0530

    n+nn+nnn

---
+++ 10compute.py
@@ -0,0 +1,11 @@
+n = int(input("enter n value : "))
+v1 = int("%s" %n)
+v2= int("%s%s" % (n,n))
+v3= int ("%s%s%s"%(n,n,n))
+print(v1+v2+v3)
+
+#output
+'''enter n value : 1
+123
+enter n value : 5
+615''

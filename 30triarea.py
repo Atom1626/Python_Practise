235b5efcbf7896a286783dfdf4e1d67557931ac5
Atom1626 <arjunpro1626@gmail.com>
2023-11-07 00:55:21 +0530

    area o triangle

---
+++ 30triarea.py
@@ -0,0 +1,12 @@
+b = float(input("enter base of triangle : "))
+h = float(input("enter height of triangle : "))
+def triangle_area(b,h):
+    return 0.5*b*h
+print(triangle_area(b,h))
+
+#oputput
+'''
+enter base of triangle : 20
+enter height of triangle : 40
+400.0
+'''

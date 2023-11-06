fd3275f24ef0885bf0b98801ffca556cb7b5f3f0
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 17:23:50 +0530

    syntax and description o function

---
+++ 11syntax.py
@@ -0,0 +1,12 @@
+print(abs.__doc__)
+print(range.__doc__)
+
+'''Return the absolute value of the argument.
+range(stop) -> range object
+range(start, stop[, step]) -> range object
+
+Return an object that produces a sequence of integers from start (inclusive)
+to stop (exclusive) by step.  range(i, j) produces i, i+1, i+2, ..., j-1.
+start defaults to 0, and stop is omitted!  range(4) produces 0, 1, 2, 3.
+These are exactly the valid indices for a list of 4 elements.
+When step is given, it specifies the increment (or decrement).'''

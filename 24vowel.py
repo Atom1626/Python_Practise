e99bf50e0c48deb158f6371b65e580954edc6bc2
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 23:45:13 +0530

    vowel or not

---
+++ 24vowel.py
@@ -0,0 +1,15 @@
+a = str(input("enter a letter : "))
+def vowel(a):
+    if a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U' :
+        print("vowel")
+    else:
+        print("consonant")
+vowel(a)
+
+#output
+'''
+enter a letter : a
+vowel
+enter a letter : w
+consonant
+'''

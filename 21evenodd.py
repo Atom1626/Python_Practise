96f8879f63b0ec981114f5b7fe9d6a9105c08adf
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 21:40:45 +0530

    evenodd

---
+++ 21evenodd.py
@@ -0,0 +1,16 @@
+n = int(input("enter number :"))
+def even_odd(n):
+    if n%2==0:
+        print(n," is even number")
+    else:
+        print(n," is odd number")
+even_odd(n)
+
+#output
+'''
+enter number :2
+2  is even number
+
+enter number :3
+3  is odd number
+'''

78c972331f364636ae53b45ab2b584c64fc6d937
Atom1626 <arjunpro1626@gmail.com>
2023-11-06 17:30:38 +0530

    calender

---
+++ 12calender.py
@@ -0,0 +1,16 @@
+import calendar
+month = int(input("enter month :"))
+year = int(input("enter year :"))
+print(calendar.month(year,month))
+
+#output
+'''enter month :01
+enter year :2000
+    January 2000
+Mo Tu We Th Fr Sa Su
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31'''
